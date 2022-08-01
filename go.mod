09fe49821684ab84eb7ae778e50b357d632b24aa
pre <preimmortal@gmail.com>
2022-08-01 09:19:33 +0000

    add yaml parser

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/preimmortal/sidecar-backup
 
 go 1.18
+
+require github.com/sirupsen/logrus v1.9.0
+
+require (
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
-
+import (
+	log "github.com/sirupsen/logrus"
+	yaml "gopkg.in/yaml.v2"
+)
+type Person struct {
+	Name string `yaml:"name"`
+	Age int `yaml:"age"`
+}
 // Main function
 func main() {
-    fmt.Println("!... Hello World ...!")
+  log.Info("Starting Sidecar-Backup")
+	var y []byte
+	var err error
+	p := &Person{
+		"abc",
+		3,
+	}
+	if y, err = yaml.Marshal(p); err != nil {
+		log.Error(err)
+	} 
+	log.Info(string(y))
 }
