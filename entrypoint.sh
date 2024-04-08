c1117318f1d8e3f18b45acadfb3a9e260f33402a
preimmortal <preimmortal@gmail.com>
2024-04-07 17:45:22 -0700

    add ability to run as nonroot container

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,26 +4,30 @@
 # Setup Environment
 #########################################################################
 echo "Setting up environment"
-
-if [ "${PGID}" == "0" ]; then
-  export USERGROUP=root
+echo "Current User: ${UID}"
+if [ "${UID}" == "0" ]; then
+  if [ "${PGID}" == "0" ]; then
+    export USERGROUP=root
+  else
+    echo "Creating Group"
+    export USERGROUP=backup
+    addgroup -g ${PGID} ${USERGROUP}
+  fi
+  
+  if [ "${PUID}" == "0" ]; then
+    export USER=root
+  else
+    echo "Creating User"
+    export USER=backup
+    adduser \
+      --disabled-password \
+      --gecos "" \
+      --ingroup "${USERGROUP}" \
+      --uid "${PUID}" \
+      "${USER}"
+  fi
+  su ${USER} -c "sidecar-backup $@"
 else
-  echo "Creating Group"
-  export USERGROUP=backup
-  addgroup -g ${PGID} ${USERGROUP}
+  sidecar-backup $@
 fi
 
-if [ "${PUID}" == "0" ]; then
-  export USER=root
-else
-  echo "Creating User"
-  export USER=backup
-  adduser \
-    --disabled-password \
-    --gecos "" \
-    --ingroup "${USERGROUP}" \
-    --uid "${PUID}" \
-    "${USER}"
-fi
-
-su ${USER} -c "sidecar-backup $@"
